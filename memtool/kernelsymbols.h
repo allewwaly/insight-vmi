3963d2df960909826bb30cd70db2a28d69f574fc
chrschn <chrschn@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2010-06-02 10:37:22 +0000

    - Include cleanups - Documentation

--- memtool/kernelsymbols.h
+++ memtool/kernelsymbols.h
@@ -10,29 +10,67 @@
 
 #include "symfactory.h"
 #include "typeinfo.h"
-#include <QHash>
 #include "genericexception.h"
 
 // Forward declaration
 class QIODevice;
 
-
+/**
+ * This class represents one instance of kernel symbols and provides functions
+ * to parse, save and load the symbols from files.
+ */
 class KernelSymbols
 {
 public:
+    /**
+     * Constructor
+     */
 	KernelSymbols();
 
+	/**
+	 * Destructor
+	 */
 	~KernelSymbols();
 
+	/**
+	 * Parses the kernel debugging symbols from the given device.
+	 * @param from device to parse the symbols from
+	 */
 	void parseSymbols(QIODevice* from);
+
+	/**
+	 * Parses the kernel debugging symbols from the file given by \a fileName.
+	 * @param fileName the name of the file to parse the symbols from
+	 */
 	void parseSymbols(const QString& fileName);
 
+	/**
+	 * Loads the kernel debugging symbols from the given device.
+	 * @param from device to load the symbols from
+	 */
     void loadSymbols(QIODevice* from);
+
+    /**
+     * Loads the kernel debugging symbols from the file given by \a fileName.
+     * @param fileName the name of the file to load the symbols from
+     */
     void loadSymbols(const QString& fileName);
 
+    /**
+     * Saves the kernel debugging symbols to the given device.
+     * @param to device to save the symbols to
+     */
     void saveSymbols(QIODevice* to);
+
+    /**
+     * Saves the kernel debugging symbols to the file given by \a fileName.
+     * @param fileName the name of the file to save the symbols to
+     */
     void saveSymbols(const QString& fileName);
 
+    /**
+     * @return the symbol factory that holds all kernel symbols
+     */
     const SymFactory& factory() const;
 
 private:
