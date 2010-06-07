efaedceab1b6f29e4915f6af2302ecd2e64ca826
chrschn <chrschn@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2010-06-07 17:29:19 +0000

    Reimplemented function prettyName() to return a C-style variable declaration.

--- memtool/variable.cpp
+++ memtool/variable.cpp
@@ -34,6 +34,23 @@ void Variable::setOffset(size_t offset)
 }
 
 
+QString Variable::prettyName() const
+{
+    // Shorten the type name, if required
+    QString s_typename = "(unresolved type)";
+    if (_refType) {
+        if (_refType->prettyName().isEmpty())
+            s_typename = "(anonymous type)";
+        else
+            s_typename = _refType->prettyName();
+    }
+
+    QString s_name = _name.isEmpty() ? "(none)" : _name;
+
+    return QString("%1 %2").arg(s_typename).arg(s_name);
+}
+
+
 void Variable::readFrom(QDataStream& in)
 {
     Symbol::readFrom(in);
