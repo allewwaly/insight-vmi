872eaee405322910b4a1bbc623d82fc8dcf73b22
chrschn <chrschn@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2010-06-17 12:54:32 +0000

    Show char* pointers as strings (moved from Array to Pointer class)

--- memtool/array.cpp
+++ memtool/array.cpp
@@ -50,22 +50,8 @@ QString Array::toString(QIODevice* mem, size_t offset) const
     assert(_refType != 0);
 
 	// Is this possibly a string?
-    if (_refType->type() == rtInt8) {
-        // Setup a buffer, at most 1024 bytes long
-        const int bufSize = _length > 0 ? _length+1 : 1024;
-        char* buf = new char[bufSize];
-        memset(buf, 0, bufSize);
-        // Read the data such that the result is always null-terminated
-        seek(mem, offset);
-        read(mem, buf, bufSize-1);
-        // Limit to ASCII characters
-        for (int i = 0; i < bufSize-1; i++) {
-            if (buf[i] == 0)
-                break;
-            else if ( (buf[i] & 0x80) || !(buf[i] & 0x60) ) // buf[i] >= 128 || buf[i] < 32
-                buf[i] = '.';
-        }
-        return QString("\"%1\"").arg(buf);
+    if (_refType && _refType->type() == rtInt8) {
+        return Pointer::toString(mem, offset);
     }
     else {
         if (_length >= 0) {
--- memtool/pointer.cpp
+++ memtool/pointer.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "pointer.h"
+#include <string.h>
 
 Pointer::Pointer()
 {
@@ -42,10 +43,28 @@ QString Pointer::prettyName() const
 
 QString Pointer::toString(QIODevice* mem, size_t offset) const
 {
-	if (_size == 4) {
-		return "0x" + QString::number(value<quint32>(mem, offset), 8, 16);
+    // Is this possibly a string?
+    if (_refType && _refType->type() == rtInt8) {
+        // Setup a buffer, at most 1024 bytes long
+        const int bufSize = 1024;
+        char buf[bufSize];
+        memset(buf, 0, bufSize);
+        // Read the data such that the result is always null-terminated
+        seek(mem, offset);
+        read(mem, buf, bufSize-1);
+        // Limit to ASCII characters
+        for (int i = 0; i < bufSize-1; i++) {
+            if (buf[i] == 0)
+                break;
+            else if ( (buf[i] & 0x80) || !(buf[i] & 0x60) ) // buf[i] >= 128 || buf[i] < 32
+                buf[i] = '.';
+        }
+        return QString("\"%1\"").arg(buf);
+    }
+    else if (_size == 4) {
+		return QString("0x%1").arg(value<quint32>(mem, offset), 8, 16);
 	}
 	else {
-		return "0x" + QString::number(value<quint64>(mem, offset), 16, 16);
+		return QString("0x%1").arg(value<quint64>(mem, offset), 16, 16);
 	}
 }
