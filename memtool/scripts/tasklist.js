7fc94b5978362790e16d7538aaa1f7d076018c16
chrschn <chrschn@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2010-07-12 14:04:09 +0000

    The task list can finally be output with a script.

--- memtool/scripts/tasklist.js
+++ memtool/scripts/tasklist.js
@@ -22,108 +22,50 @@ function ralign(s, len)
 	return s;
 }
 
-var uid_size = 8;
+var uid_size = 4;
 var pid_size = 8;
 var gid_size = 8;
 var cmd_size = 18;
+var addr_size = 18;
 
-function printSiblings(p)
+function printList(p)
 {
-//	if (!(p instanceof Instance)) {
-//		print("This is not an Instance object!, p = " + p);
-//		return;
-//	}
-//	print("+++ Entering printSiblings()");
-	
 	if (p.isNull()) {
-//		print("+++ Returning from printSiblings()");
 		return;
 	}
 	
 	var it = p;
-//	print("p.parent.address() = 0x" + p.parent.address().toString(16));
 	
 	do {
 		var line = 
-			lalign(it.uid.toString(), uid_size) + 
+			ralign(it.uid.toString(), uid_size) + 
 			ralign(it.pid.toString(), pid_size) + 
 			ralign(it.gid.toString(), gid_size) +
 			"  " +
 			lalign(it.comm.toString(), cmd_size) +
-			"  @ 0x" + it.address().toString(16);
+			lalign("0x" + it.address(), addr_size);
 
 		print(line);
 		
-		printSiblings(it.children.next);
-		
-		it = it.sibling.next;
-//		print ("it.address() = 0x" + it.address().toString(16) + ", ");
-	} while (it.address() != p.parent.address() && it.address() != p.address());
-
-
-//	print("+++ Leaving printSiblings()");
+		it = it.tasks.next;
+	} while (it.address() != p.address());
 }
 
-var hdr = 
-	lalign("USER", uid_size) + 
-	ralign("PID", pid_size) + 
-	ralign("GID", gid_size) +
-	"  " + 
-	lalign("COMMAND", cmd_size);
+function printHdr()
+{
+	var hdr = 
+		ralign("USER", uid_size) + 
+		ralign("PID", pid_size) + 
+		ralign("GID", gid_size) +
+		"  " + 
+		lalign("COMMAND", cmd_size) +
+		lalign("ADDRESS", addr_size);
+	
+	print(hdr);
+}
 
-print(hdr);
+var init = new Instance("init_task");
 
-var it = new Instance("init_task");
+printHdr();
+printList(init.tasks.next);
 
-//if (!(it instanceof Instance))
-//	print("Not an Instance object: " + it);
-printSiblings(it.children.next, it);
-
-//dumpThis(it.pid);
-
-
-//print("it = \"" + it.typeName() + "\" @ 0x" + it.address().toString(16));
-//print(it.pid.toString());
-//dumpInstance(it.pid);
-
-//ch = it.children;
-//dumpInstance(ch);
-
-//dumpInstance(ch.next.pid);
-
-//print("------------");
-
-
-//var names = ch.memberNames();
-//print("names = " + names);
-//
-//for (var n in names) {
-//	print(n);
-//} 
-
-// for (var i = 0; i < names.length; i++) {
-//	print(names[i]);
-//}
-
-
-//var m = ch.members();
-
-//print("The members of " + ch.fullName() + " are:");
-//for (m in ch) {
-//	print("  name: " + ch[m].fullName() + ", value: " + "...");
-//}
-//
-//print("----------");
-//
-//print(ch[m].pid.fullName());
-
-//print("children.next = " + ch["next"]);
-
-//
-//for (prop in m)
-//	print ("it = " + it);
-//
-//var a = new Array(1, 2, 3);
-//print("a = " + typeof a);
-//if (a instanceof Array)
-//	print("a is an Array");
