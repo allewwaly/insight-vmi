e700714c593b6c1b27c287848e43438961f1fef2
chrschn <chrschn@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2010-07-12 14:03:40 +0000

    - Bugfix: Don't return any q(u)int64 values to the script, as they get automatically casted to double values. Instead, provide xxxHigh() and xxxLow() functions to access 64 bit integers natively. - Added almost all toXXX() functions from BaseType to Instance and InstancePrototype so that they are available in the scripting environment.

--- trunk/memtool/instanceprototype.h
+++ trunk/memtool/instanceprototype.h
@@ -21,6 +21,16 @@
  * the function invokation to the underlying Instance object, obtained by
  * thisInstance().
  *
+ * Only exceptions: As ECMA-262 only supports \c int32 and \c uint32, a
+ * \c (u)int64 in QtScript is automatically castet to a \a double value, which
+ * leads to a loss of precision. The only value that is affected by this
+ * problem is the instance's address. We handle this problem by providing three
+ * access functions to the address:
+ *
+ * \li address() returns the address as zero-padded string in hex format
+ * \li addressHigh() returns the most significant 32 bits of the address as \c uint32
+ * \li addressLow() returns the least significant 32 bits of the address as \c uint32
+ *
  * \sa Instance
  * \sa InstanceClass
  */
@@ -33,19 +43,48 @@ public:
 
 public slots:
 	bool isNull() const;
-    quint64 address() const;
+
+	/**
+	 * @return the address as zero-padded string in hex format
+	 */
+    QString address() const;
+
+    /**
+     * @return the most significant 32 bits of the address as \c uint32
+     */
+    quint32 addressHigh() const;
+
+    /**
+     * @return the least significant 32 bits of the address as \c uint32
+     */
+    quint32 addressLow() const;
+
     QString name() const;
     QString parentName() const;
     QString fullName() const;
     QStringList memberNames() const;
     InstanceList members() const;
-//    const BaseType* type() const;
+    QString type() const;
     QString typeName() const;
     quint32 size() const;
     bool memberExists(const QString& name) const;
     Instance findMember(const QString& name) const;
     int typeIdOfMember(const QString& name) const;
+
+    qint8 toInt8() const;
+    quint8 toUInt8() const;
+    qint16 toInt16() const;
+    quint16 toUInt16() const;
+    qint32 toInt32() const;
+    quint32 toUInt32() const;
+    QString toInt64(int base = 10) const;
+    QString toUInt64(int base = 10) const;
+    quint32 toUInt64High() const;
+    quint32 toUInt64Low() const;
+    float toFloat() const;
+    double toDouble() const;
     QString toString() const;
+    int pointerSize() const;
 
 private:
     inline Instance* thisInstance() const;
