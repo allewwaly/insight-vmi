e09c05cc84a89b00fe49a9d5d33b83643e8faba8
chrschn <chrschn@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2010-07-22 17:07:59 +0000

    Fixed the script and beautified the output.

--- memtool/scripts/globalvars.js
+++ memtool/scripts/globalvars.js
@@ -1,19 +1,52 @@
 
-//var i = new Instance(0x2a48f);
-//
-//print(i.toString());
+function lalign(s, len)
+{
+	while (len > 0 && s.length < len)
+		s += " ";
+	return s;
+}
+
+function ralign(s, len)
+{
+	while (len > 0 && s.length < len)
+		s = " " + s;
+	return s;
+}
+
 
 
 // Retrieve list of all global variable names
 var varIds = getVariableIds();
 
+// What is the max. ID?
+var maxId = varIds.length > 0 ? varIds[varIds.length - 1] : 0;
+
+var size_name = 40;
+var size_typeName = 25;
+var size_type = 13;
+var size_id = 1;
+while (maxId > 0) {
+	maxId >>= 4;
+	size_id++;
+}
+
+
+// Iterate over all variable IDs
 for (var i = 0; i < varIds.length; ++i) {
-	print(i + ": 0x" + varIds[i].toString(16));
+//	print(i + ": 0x" + varIds[i].toString(16));
 	// Create an instance of each variable
 	var inst = new Instance(varIds[i]);
-	// Output some information about it
-	print("0x" + inst.Id().toString(16) + " " + 
-			inst.Name() + ": " + inst.TypeName() + " @ 0x" + inst.Address() + 
-			", size = " + inst.Size() + " bytes, real type = " + inst.Type());
+	// Collect some information about it
+	var name = lalign(inst.Name(), size_name);
+	var typeName = name.length > size_name ? 
+			inst.TypeName() : 
+			lalign(inst.TypeName(), size_typeName)
+	// Output the information
+	print(ralign("0x" + inst.Id().toString(16), size_id) + " " + 
+		  lalign(name + ": " + typeName,
+				 size_name + size_typeName + 2) + 
+		  " " + 
+		  lalign("(" + inst.Type() + ")", size_type) +
+		  " @ 0x" + inst.Address()); 
 }
 
