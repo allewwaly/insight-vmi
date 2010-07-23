1cc40d806eaae204158504cfd3a3a0590c0bc9c6
chrschn <chrschn@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2010-07-23 17:37:38 +0000

    Added exception handling to script, now it works perfectly. :-)

--- trunk/memtool/scripts/globalvars.js
+++ trunk/memtool/scripts/globalvars.js
@@ -1,4 +1,5 @@
 
+// Prints a string with a given width left-aligned
 function lalign(s, len)
 {
 	while (len > 0 && s.length < len)
@@ -6,6 +7,7 @@ function lalign(s, len)
 	return s;
 }
 
+//Prints a string with a given width right-aligned
 function ralign(s, len)
 {
 	while (len > 0 && s.length < len)
@@ -24,12 +26,12 @@ var maxId = varIds.length > 0 ? varIds[varIds.length - 1] : 0;
 var size_name = 40;
 var size_typeName = 25;
 var size_type = 13;
+var size_val = 20;
 var size_id = 1;
 while (maxId > 0) {
 	maxId >>= 4;
 	size_id++;
 }
-
 
 // Iterate over all variable IDs
 for (var i = 0; i < varIds.length; ++i) {
@@ -41,12 +43,38 @@ for (var i = 0; i < varIds.length; ++i) {
 	var typeName = name.length > size_name ? 
 			inst.TypeName() : 
 			lalign(inst.TypeName(), size_typeName)
+	var val;
+	try {
+		if (inst.Type() == "Struct" || inst.Type() == "Union")
+			val = "...";
+		else {
+			val = inst.toString();
+			if (val.length > size_val)
+				val = val.substr(0, size_val - 3) + "...";
+		}
+	}
+	catch (err) {
+		val = "(not accessible)"
+	}
+			
 	// Output the information
 	print(ralign("0x" + inst.Id().toString(16), size_id) + " " + 
 		  lalign(name + ": " + typeName,
 				 size_name + size_typeName + 2) + 
 		  " " + 
 		  lalign("(" + inst.Type() + ")", size_type) +
-		  " @ 0x" + inst.Address()); 
+		  " @ 0x" + inst.Address() +
+		  " = " +
+		  lalign(val, size_val));
+
+//    try {
+//		print(inst.toString());
+//    }
+//    catch (err) {
+//    	print(err);
+//    }
+			
 }
+
+print("Total number of global variables: " + varIds.length)
 
