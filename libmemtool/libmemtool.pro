7682e003bbb6a5b11ece7778669ce4036587c573
chrschn <chrschn@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2010-08-02 15:44:59 +0000

    Initial interface library for memtool daemon. Still work in progress.

--- libmemtool/libmemtool.pro
+++ libmemtool/libmemtool.pro
@@ -1,9 +1,16 @@
 TEMPLATE = lib
-TARGET = libmemtool 
+TARGET = memtool
+VERSION = 1.0.0
 DEFINES += LIBMEMTOOL_LIBRARY
-
-QT        += core network 
-QT        -= gui
-
-HEADERS   +=
-SOURCES   += main.cpp
+QT += core \
+    network
+QT -= gui
+HEADERS += include/memtool/memtool.h \
+    sockethelper.h \
+    include/memtool/memtoolexception.h \
+    include/memtool/constdefs.h
+SOURCES += sockethelper.cpp \
+    memtoolexception.cpp \
+    memtool.cpp \
+    constdefs.cpp
+INCLUDEPATH += ./include
---
+++ libmemtool/sockethelper.cpp
@@ -0,0 +1,36 @@
+/*
+ * sockethelper.cpp
+ *
+ *  Created on: 02.08.2010
+ *      Author: chrschn
+ */
+
+#include "sockethelper.h"
+#include <QLocalSocket>
+
+
+SocketHelper::SocketHelper(QLocalSocket* socket, QObject* parent)
+    : QObject(parent), _socket(socket)
+{
+    // Connect the signals
+    connect(_socket, SIGNAL(readyRead()), SLOT(handleReadyRead()));
+}
+
+
+void SocketHelper::reset()
+{
+    _data.clear();
+}
+
+
+QByteArray& SocketHelper::data()
+{
+    return _data;
+}
+
+
+void SocketHelper::handleReadyRead()
+{
+    _data += _socket->readAll();
+}
+
