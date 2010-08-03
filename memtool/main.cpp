02b2ce92289ed4d0aae329406fde8ab47c9da101
chrschn <chrschn@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2010-08-03 15:50:24 +0000

    Tried to create a controller for the memtoold daemon, but the socket communication isn't working yet because of strange Qt errors. :-(

--- memtool/main.cpp
+++ memtool/main.cpp
@@ -1,9 +1,23 @@
 #include <QtCore>
 #include <QCoreApplication>
 
+#include "programoptions.h"
+#include "shell.h"
+
+
+
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-	
-    return a.exec();
+    QCoreApplication app(argc, argv);
+
+    // Parse the command line options
+    QStringList args = app.arguments();
+    args.pop_front();
+    if (!programOptions.parseCmdOptions(args))
+        return 1;
+
+    shell = new Shell(false);
+    shell->start();
+
+    return app.exec();
 }
