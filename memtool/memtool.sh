c128441305e8ede41d6464c8668527eb48a29e0f
chrschn <chrschn@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2010-08-12 17:23:38 +0000

    Set the PATH to include the memtoold directory, if it exists.

--- memtool/memtool.sh
+++ memtool/memtool.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LIB="libmemtool.so.1"
-
+DAEMON="memtoold"
 
 if [ -r ../libmemtool/$LIB ]; then
 	export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+LD_LIBRARY_PATH:}../libmemtool"
@@ -10,6 +10,10 @@ elif [ -r ./$LIB ]; then
 else
 	echo "Cannot find $LIB, aborting." >&2
 	exit 1
+fi
+
+if [ -x ../memtoold/$DAEMON ]; then
+	export PATH="../memtoold:$PATH"
 fi
 
 exec ./memtool "$@"
