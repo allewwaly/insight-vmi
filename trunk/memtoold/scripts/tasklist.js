ea3a6d70a3ba03c2185763801b8797d42d43b6b4
vogls <vogls@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2010-08-23 07:45:34 +0000

    Addes some scripts that can be used to print the debug registers or view some of the currently running processes using the SLAB allocator.

--- trunk/memtoold/scripts/tasklist.js
+++ trunk/memtoold/scripts/tasklist.js
@@ -25,6 +25,7 @@ function ralign(s, len)
 var uid_size = 4;
 var pid_size = 8;
 var gid_size = 8;
+var state_size = 8;
 var cmd_size = 18;
 var addr_size = 18;
 
@@ -41,6 +42,7 @@ function printList(p)
 			ralign(it.uid.toString(), uid_size) + 
 			ralign(it.pid.toString(), pid_size) + 
 			ralign(it.gid.toString(), gid_size) +
+			ralign(it.state.toString(), state_size) +
 			"  " +
 			lalign(it.comm.toString(), cmd_size) +
 			lalign("0x" + it.Address(), addr_size);
@@ -57,6 +59,7 @@ function printHdr()
 		ralign("USER", uid_size) + 
 		ralign("PID", pid_size) + 
 		ralign("GID", gid_size) +
+		ralign("STATE", state_size) +
 		"  " + 
 		lalign("COMMAND", cmd_size) +
 		lalign("ADDRESS", addr_size);
@@ -67,5 +70,5 @@ function printHdr()
 var init = new Instance("init_task");
 
 printHdr();
-printList(init.tasks.next);
+printList(init);
 
