f5ba287f8ee3889b0b58f6f24769f2bbd36689ae
chrschn <chrschn@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2010-12-23 15:10:55 +0000

    More robust library finding

--- memtoold/memtoold.sh
+++ memtoold/memtoold.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 LIB="libmemtool.so.1"
+BASE="$(dirname $0)"
 
-
-if [ -r ../libmemtool/$LIB ]; then
-	export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+LD_LIBRARY_PATH:}../libmemtool"
-elif [ -r ./$LIB ]; then
-	export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+LD_LIBRARY_PATH:}."
+if [ -r "$BASE/../libmemtool/$LIB" ]; then
+	export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+LD_LIBRARY_PATH:}$BASE/../libmemtool"
+elif [ -r "$BASE/$LIB" ]; then
+	export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+LD_LIBRARY_PATH:}$BASE"
 else
 	echo "Cannot find $LIB, aborting." >&2
 	exit 1
 fi
 
-exec ./memtoold "$@"
+exec "$BASE/memtoold" "$@"
