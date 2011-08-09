520da816a37573a80b79e19826d2ad37fe319a34
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2011-08-09 17:36:22 +0000

    Updated documentation.

--- insightd/kernelsymbolsclass.h
+++ insightd/kernelsymbolsclass.h
@@ -22,6 +22,9 @@ class VirtualMemory;
 /**
  * This class provides access to the kernel symbols from within the QtScript
  * environment.
+ *
+ * \note All methods of this class implemented as Qt slots are available within
+ * the scripting environment using the global \c Symbols object.
  */
 class KernelSymbolsClass: public QObject, public QScriptable
 {
@@ -33,6 +36,10 @@ public:
 	 * @param parent parent object
 	 */
 	KernelSymbolsClass(InstanceClass* instClass, QObject* parent = 0);
+
+	/**
+	 * Destructor
+	 */
 	virtual ~KernelSymbolsClass();
 
 public slots:
@@ -45,6 +52,15 @@ public slots:
 	 * @param index the index of the memory file to use for the resulting
 	 * instances, defaults to -1 which uses the first available, if any
 	 * @return an array with all types matching \a filter (if specified)
+	 *
+	 * Example usage within a script:
+	 * \code
+	 * var list = Symbols.listTypes("list_head");
+	 * for (var i in list) {
+	 *     print(i + " " + list[i].TypeName() +
+	 *         ", size=" + list[i].Size());
+	 * }
+	 * \endcode
 	 */
 	QScriptValue listTypes(QString filter = QString(), int index = -1);
 
@@ -56,6 +72,16 @@ public slots:
 	 * @param index the index of the memory file to use for the resulting
 	 * instances, defaults to -1 which uses the first available, if any
 	 * @return an array with all variables matching \a filter (if specified)
+	 *
+     * Example usage within a script:
+     * \code
+     * var list = Symbols.listVariables("init_*");
+     * for (var i in list) {
+     *     print(i + " " + list[i].Name() +
+     *         ": " + list[i].TypeName() +
+     *         " @ 0x" + list[i].Address().toString(16));
+     * }
+     * \endcode
 	 */
 	QScriptValue listVariables(QString filter = QString(), int index = -1);
 
