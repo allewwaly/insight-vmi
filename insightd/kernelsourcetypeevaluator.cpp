f54ac602a8158b10fb7b4199d114362b9a6a47e5
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2011-09-15 23:06:47 +0000

    More readable output for debugging.

--- insightd/kernelsourcetypeevaluator.cpp
+++ insightd/kernelsourcetypeevaluator.cpp
@@ -32,18 +32,35 @@ void KernelSourceTypeEvaluator::primaryExpressionTypeChange(
         const ASTNode* srcNode, const ASTType* srcType,
         const ASTSymbol& srcSymbol, const ASTType* ctxType,
         const ASTNode* ctxNode, const QStringList& ctxMembers,
-        const ASTNode* targetNode, const ASTType* targetType)
+        const ASTNode* targetNode, const ASTType* targetType,
+        const ASTNode* rootNode)
 {
-    const ASTType* ctxTypeNonPtr = 0;
-    QList<BaseType*> srcBaseTypes;
-
+    ASTSourcePrinter printer(_ast);
     // Ignore all usages of a pointer as an integer, we cannot learn anything
     // from that
     if (targetType->type() != rtPointer) {
-//        debugmsg("Skipping type change where target type is no pointer:");
-//        ASTTypeEvaluator::primaryExpressionTypeChange(
-//                    srcNode, srcType, srcSymbol, ctxType, ctxNode, ctxMembers,
-//                    targetNode, targetType);
+        debugmsg(QString("%9:\n\tSymbol: %1 (%2)\n\tSource: %3, %4\n\tTarget: %5, %6\n\tLine %7")
+                 .arg(srcSymbol.name(), -30)
+                 .arg(srcSymbol.typeToString())
+                 .arg(printer.toString(srcNode, false).trimmed(), -30)
+                 .arg(srcType->toString())
+                 .arg(printer.toString(targetNode, false).trimmed(), -30)
+                 .arg(targetType->toString())
+                 .arg(printer.toString(rootNode, true).trimmed())
+                 .arg("Target is no pointer"));
+        return;
+    }
+    // Ignore parameters of non-struct types as source
+    if (srcSymbol.type() == stFunctionParam && ctxMembers.isEmpty()) {
+        debugmsg(QString("%9:Symbol: %1 (%2)\n\tSource: %3, %4\n\tTarget: %5, %6\n\tLine %7")
+                 .arg(srcSymbol.name(), -30)
+                 .arg(srcSymbol.typeToString())
+                 .arg(printer.toString(srcNode, false).trimmed(), -30)
+                 .arg(srcType->toString())
+                 .arg(printer.toString(targetNode, false).trimmed(), -30)
+                 .arg(targetType->toString())
+                 .arg(printer.toString(rootNode, true).trimmed())
+                 .arg("Source is non-struct paramter"));
         return;
     }
 
@@ -60,12 +77,12 @@ void KernelSourceTypeEvaluator::primaryExpressionTypeChange(
         ASTSourcePrinter printer(_ast);
         std::cout
                 << "------------------[Source]------------------" << std::endl
-                << printer.toString(const_cast<ASTNode*>(n), true)
+                << printer.toString(n, true)
                 << "------------------[/Source]-----------------" << std::endl;
 
         ASTTypeEvaluator::primaryExpressionTypeChange(
                     srcNode, srcType, srcSymbol, ctxType, ctxNode, ctxMembers,
-                    targetNode, targetType);
+                    targetNode, targetType, rootNode);
         throw e;
     }
 }
