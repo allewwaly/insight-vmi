54d090044933e7fff961f8ed1114c361c9e65f7e
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2011-09-15 23:57:47 +0000

    Refactorings.

--- insightd/kernelsourcetypeevaluator.cpp
+++ insightd/kernelsourcetypeevaluator.cpp
@@ -28,6 +28,27 @@ KernelSourceTypeEvaluator::~KernelSourceTypeEvaluator()
 }
 
 
+QString KernelSourceTypeEvaluator::typeChangeInfo(
+        const ASTNode* srcNode, const ASTType* srcType,
+        const ASTSymbol& srcSymbol, const ASTNode* targetNode,
+        const ASTType* targetType, const ASTNode* rootNode)
+{
+    ASTSourcePrinter printer(_ast);
+#   define INDENT "    "
+    return QString(INDENT "Symbol: %1 (%2)\n"
+                   INDENT "Source: %3 %4\n"
+                   INDENT "Target: %5 %6\n"
+                   INDENT "Line %7")
+            .arg(srcSymbol.name(), -30)
+            .arg(srcSymbol.typeToString())
+            .arg(printer.toString(srcNode, false).trimmed() + ",", -30)
+            .arg(srcType->toString())
+            .arg(printer.toString(targetNode, false).trimmed() + ",", -30)
+            .arg(targetType->toString())
+            .arg(printer.toString(rootNode, true).trimmed());
+}
+
+
 void KernelSourceTypeEvaluator::primaryExpressionTypeChange(
         const ASTNode* srcNode, const ASTType* srcType,
         const ASTSymbol& srcSymbol, const ASTType* ctxType,
@@ -35,32 +56,29 @@ void KernelSourceTypeEvaluator::primaryExpressionTypeChange(
         const ASTNode* targetNode, const ASTType* targetType,
         const ASTNode* rootNode)
 {
-    ASTSourcePrinter printer(_ast);
+    Q_UNUSED(ctxNode);
     // Ignore all usages of a pointer as an integer, we cannot learn anything
     // from that
     if (targetType->type() != rtPointer) {
-        debugmsg(QString("%9:\n\tSymbol: %1 (%2)\n\tSource: %3, %4\n\tTarget: %5, %6\n\tLine %7")
-                 .arg(srcSymbol.name(), -30)
-                 .arg(srcSymbol.typeToString())
-                 .arg(printer.toString(srcNode, false).trimmed(), -30)
-                 .arg(srcType->toString())
-                 .arg(printer.toString(targetNode, false).trimmed(), -30)
-                 .arg(targetType->toString())
-                 .arg(printer.toString(rootNode, true).trimmed())
-                 .arg("Target is no pointer"));
+        debugmsg("Target is no pointer:\n" +
+                 typeChangeInfo(srcNode, srcType, srcSymbol, targetNode,
+                                targetType, rootNode));
         return;
     }
     // Ignore parameters of non-struct types as source
     if (srcSymbol.type() == stFunctionParam && ctxMembers.isEmpty()) {
-        debugmsg(QString("%9:Symbol: %1 (%2)\n\tSource: %3, %4\n\tTarget: %5, %6\n\tLine %7")
-                 .arg(srcSymbol.name(), -30)
-                 .arg(srcSymbol.typeToString())
-                 .arg(printer.toString(srcNode, false).trimmed(), -30)
-                 .arg(srcType->toString())
-                 .arg(printer.toString(targetNode, false).trimmed(), -30)
-                 .arg(targetType->toString())
-                 .arg(printer.toString(rootNode, true).trimmed())
-                 .arg("Source is non-struct paramter"));
+        debugmsg("Source is non-struct paramter:\n" +
+                 typeChangeInfo(srcNode, srcType, srcSymbol, targetNode,
+                                targetType, rootNode));
+        return;
+    }
+    // Ignore values return by functions
+    if (srcSymbol.type() == stFunctionDef ||
+        srcSymbol.type() == stFunctionDecl)
+    {
+        debugmsg("Source is return value of function invocation:\n" +
+                 typeChangeInfo(srcNode, srcType, srcSymbol, targetNode,
+                                targetType, rootNode));
         return;
     }
 
@@ -80,9 +98,8 @@ void KernelSourceTypeEvaluator::primaryExpressionTypeChange(
                 << printer.toString(n, true)
                 << "------------------[/Source]-----------------" << std::endl;
 
-        ASTTypeEvaluator::primaryExpressionTypeChange(
-                    srcNode, srcType, srcSymbol, ctxType, ctxNode, ctxMembers,
-                    targetNode, targetType, rootNode);
+        std::cout << typeChangeInfo(srcNode, srcType, srcSymbol, targetNode,
+                                    targetType, rootNode);
         throw e;
     }
 }
