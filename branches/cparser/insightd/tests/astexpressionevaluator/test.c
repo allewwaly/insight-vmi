438f471802fdead36beda386b607107820a314b0
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2011-11-08 17:33:11 +0000

    Tests for the expresison evaluator.

---
+++ branches/cparser/insightd/tests/astexpressionevaluator/test.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+
+struct A {
+	long l;
+	short s;
+	int i;
+	char c;
+};
+
+struct B {
+	int j;
+	struct A a[4];
+	struct A* pa;
+	struct B* pb;
+};
+
+struct A a;
+struct B b;
+
+int main(int argc, char** argv)
+{
+	a.i = 0;
+	b.a[0].i = 0;
+	return a.i + b.a[0].i;
+}
