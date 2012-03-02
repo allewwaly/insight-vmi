731c11bfb6eebf90137a27cdd3b4ebd37cab029f
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2012-03-02 19:36:59 +0000

    Added script to list types whose members have alternative candidate types.

---
+++ insightd/scripts/altTypes.js
@@ -0,0 +1,58 @@
+
+include("lib_string.js");
+
+var w_id = 8;
+var w_name = 40;
+var w_member = 20;
+var w_alt = 4;
+var w_total = w_id + w_name + w_member + w_alt + 3;
+
+var minAlt = (ARGV.length > 1) ? ARGV[1] : 1;
+
+
+print("Searching struct members with more than " + minAlt.toString() + " candidates.");
+print();
+print(
+	lalign("ID", w_id) + " " +
+	lalign("Type name", w_name) + " " +
+	lalign("Member", w_member) + " " +
+	ralign("Alt.", w_alt)
+);
+print(hline(w_total));
+
+// Get all type names
+var types = Symbols.listTypes();
+var tcount = 0;
+var mcount = 0;
+var lastType = 0;
+
+for (var i in types) {
+	// Get instance of that type
+	var type = types[i];
+	// We are only looking for structs with members
+	if (type.MemberCount() <= 0)
+		continue;
+
+	for (var j = 0; j < type.MemberCount(); ++j) {
+		// Skip members without candidates
+		if (type.MemberCandidatesCount(j) < minAlt)
+			continue;
+
+		// Count types with candidates
+		if (lastType != type.TypeId()) {
+			lastType = type.TypeId();
+			++tcount;
+		}
+
+		print(
+			lalign(uhex(type.TypeId()), w_id) + " " +
+			lalign(type.TypeName(), w_name) + " " +
+			lalign(type.MemberNames()[j], w_member) + " " +
+			ralign(type.MemberCandidatesCount(j), w_alt)
+		);
+		++mcount
+	}
+}
+
+print(hline(w_total));
+print("Total: " + mcount + " members in " + tcount + " types");
--- insightd/scripts/lib_string.js
+++ insightd/scripts/lib_string.js
@@ -24,3 +24,16 @@ function hline(len)
 		s += "-";
 	return s;
 }
+
+// Returns an unsigned hex representation of the given integer
+function uhex(i)
+{
+	// Positive integers can be converted directly
+	if (i >= 0)
+		return i.toString(16);
+	// The least significant 28 bit are easy
+	var back = i & 0x0fffffff;
+	// Shift the most significant bits without sign extension
+	var front = i >>> 28;
+	return front.toString(16) + back.toString(16);
+}
