d869f3e545b8b30920afbc92f8bac07a8df4c8c2
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2012-03-07 17:05:02 +0000

    Iterate over modules using the parsed candidate type.

--- branches/cparser/insightd/scripts/modules.js
+++ branches/cparser/insightd/scripts/modules.js
@@ -7,18 +7,28 @@ function printModuleList()
 {
 	var head = new Instance("modules");
 	var m = head.next;
-	m.ChangeType("module");
+	m.ChangeType("module");	
 
 	// Offset we have to use for correction
 	var offset = m.MemberOffset("list");
 	m.AddToAddress(-offset);
-	
+
 	// Prepare head for correct loop terminaten
 	head.AddToAddress(-offset);
 	
 	do {
 		print(m.name.toString());
-		m = m.list.next;
+		// Automated through source code parser
+		if (m.list.MemberCandidatesCount("next") > 0) {
+			m = m.list.MemberCandidate("next", 0);
+		}
+		// Manually adjust type and offset work
+		else {
+			// Maybe throw an exception here?
+			m = m.list.next;
+			m.ChangeType("module");
+			m.AddToAddress(-offset);
+		}
 	} while (m && m.Address() != head.Address());
 }
 
