54b07ad63b7663e16f3f6b4a2c4fec24ed12258e
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2012-03-09 01:42:45 +0000

    - More intuitive loop termination - Output the modules that use a loaded module - Nicer output in columns

--- branches/cparser/insightd/scripts/modules.js
+++ branches/cparser/insightd/scripts/modules.js
@@ -3,19 +3,37 @@
  *
  */
 
+include("lib_string.js");
+
 function printModuleList()
 {
     var head = new Instance("modules");
 
-    if (head.MemberCandidatesCount("next") <= 1)
+    if (head.MemberCandidatesCount("next") < 1)
         throw new Error("\"" + head.Name() + "\" does not have any candidate types for member \"next\"");
 
-    var m = head.MemberCandidate("next", 0);
-    print(m.name.toString());
+    var w_mod = 20;
+    print(lalign("Module", w_mod) + " Used by");
 
-	while (m && m.list.next.Address() != head.Address()) {
-		m = m.list.MemberCandidate("next", 0);
-		print(m.name.toString());
+	// Iterate over all modules
+	var m = head.next;
+	while (m && m.MemberAddress("list", true) != head.Address()) {
+		// Build list of modules which use the current module
+		var usedList = "";
+		var u = m.modules_which_use_me.next;
+		while (u.MemberAddress("list", true) != m.modules_which_use_me.Address()) {
+			if (usedList.length > 0)
+				usedList = usedList + ", ";
+			usedList = usedList + u.module_which_uses.name.toString();
+			u = u.list.next;
+		}
+
+		if (usedList.length > 0)
+			print(lalign(m.name.toString(), w_mod) + " " + usedList);
+		else
+			print(m.name.toString());
+
+		m = m.list.next;
 	}
 }
 
