541c447fdd93c2e340d9672a9f80a5140751c7ce
vogls@in.tum.de <vogls@in.tum.de@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2012-07-25 12:28:32 +0000

    Added a helper class to ease the debugging of the memory map.

---
+++ branches/revmap/insightd/memorymapverifier.cpp
@@ -0,0 +1,257 @@
+#include "memorymapverifier.h"
+
+#include <QFile>
+#include <QProcess>
+#include <QStringList>
+#include <QTextStream>
+#include <debug.h>
+
+#include <math.h>
+
+
+/*
+ * ===================================> MemoryMapNodeWatcher
+ */
+MemoryMapNodeWatcher::MemoryMapNodeWatcher(MemoryMapNode *node,
+                                           MemoryMapVerifier &verifier,
+                                           bool forcesHalt) :
+    _node(node), _verifier(verifier), _forcesHalt(forcesHalt)
+{
+
+}
+
+bool MemoryMapNodeWatcher::getForceHalt() const
+{
+    return _forcesHalt;
+}
+
+const QString MemoryMapNodeWatcher::failMessage(MemoryMapNode *lastNode)
+{
+    MemoryMapNode *lca = _verifier.leastCommonAncestor(lastNode, _node);
+
+    if(lca == NULL && lastNode == NULL) {
+       return QString("Check failed.\n"
+               "Node: %1 (%2) (%3)\n"
+               "Last Node processed: NULL!")
+                .arg(_node->fullName())
+                .arg(_node->type()->prettyName())
+                .arg(_node->probability(), 0, 'f', 3);
+    }
+    else if(lastNode == NULL) {
+        return QString("Check failed.\n"
+                       "Node: %1 (%2) (%3)\n"
+                       "Last Node processed: NULL!")
+                        .arg(_node->fullName())
+                        .arg(_node->type()->prettyName())
+                .arg(_node->probability(), 0, 'f', 3);
+    }
+    else if(lca == NULL) {
+        return QString("Check failed.\n"
+                       "Node: %1 (%2) (%3)\n"
+                       "Last Node processed: %4 (%5) (%6)\n"
+                       "No Common Ancestor!")
+                        .arg(_node->fullName())
+                        .arg(_node->type()->prettyName())
+                        .arg(_node->probability(), 0, 'f', 3)
+                        .arg(lastNode->fullName())
+                        .arg(lastNode->type()->prettyName())
+                        .arg(lastNode->probability(), 0, 'f', 3);
+    }
+    else {
+        return QString("Check failed.\n"
+                       "Node: %1 (%2) (%3)\n"
+                       "Last Node processed: %4 (%5) (%6)\n"
+                       "Common Ancestor: %7 (%8) (%9)")
+                        .arg(_node->fullName())
+                        .arg(_node->type()->prettyName())
+                        .arg(_node->probability(), 0, 'f', 3)
+                        .arg(lastNode->fullName())
+                        .arg(lastNode->type()->prettyName())
+                        .arg(lastNode->probability(), 0, 'f', 3)
+                        .arg(lca->fullName())
+                        .arg(lca->type()->prettyName())
+                        .arg(lca->probability(), 0, 'f', 3);
+    }
+}
+/*
+ *  MemoryMapNodeWatcher ===================================>
+ */
+
+
+/*
+ * ===================================> MemoryMapNodeIntervalWatcher
+ */
+MemoryMapNodeIntervalWatcher::MemoryMapNodeIntervalWatcher(MemoryMapNode *node,
+                                                           MemoryMapVerifier &verifier,
+                                                           float changeInterval,
+                                                           bool forcesHalt,
+                                                           bool flexibleInterval) :
+    MemoryMapNodeWatcher(node, verifier, forcesHalt), _changeInterval(changeInterval),
+    _lastProbability(node->probability()), _flexibleInterval(flexibleInterval)
+{
+}
+
+bool MemoryMapNodeIntervalWatcher::check()
+{
+    bool result = false;
+
+    if(_changeInterval > (fabs(_node->probability() - _lastProbability)))
+        result = true;
+
+    if(_flexibleInterval)
+        _lastProbability = _node->probability();
+
+    return result;
+}
+
+/*
+ *  MemoryMapNodeIntervalWatcher ===================================>
+ */
+
+
+/*
+ * ===================================> MemoryMapVerifier
+ */
+MemoryMapVerifier::MemoryMapVerifier() :
+    _log("memory_map", true),
+    _lastVerification(true)
+{
+}
+
+MemoryMapVerifier::MemoryMapVerifier(const char *slubFile) :
+    _log("memory_map", true),
+    _lastVerification(true)
+{
+    parseSlubFile(slubFile);
+}
+
+void MemoryMapVerifier::parseSlubFile(const char *slubFile)
+{
+    QRegExp adr("0x([a-fA-F0-9]+)");
+    QFile slub(slubFile);
+    bool err = false;
+
+    _log.debug("Parsing slub data... ");
+
+    if(!slub.open(QIODevice::ReadOnly)) {
+        _log.error(QString("Could not open file '%1'").arg(slubFile));
+        return;
+    }
+
+    QTextStream in(&slub);
+
+    while(!in.atEnd()) {
+        QString line = in.readLine();
+        QStringList lineParts = line.split(" ", QString::SkipEmptyParts);
+
+        if(lineParts.size() == 2 && adr.exactMatch(lineParts.at(1).trimmed())) {
+            _slubData[lineParts.at(1).trimmed().toULongLong(&err, 16)] = lineParts.at(0).trimmed();
+
+            if(!err)
+                _log.error(QString("Conversion of '%1' to ULongLong failed!").arg(lineParts.at(1)));
+        }
+    }
+
+    _log.debug(QString("Parsed %1 slub entries").arg(_slubData.size()));
+}
+
+
+bool MemoryMapVerifier::verifyAddress(quint64 address)
+{
+    return _slubData.contains(address);
+}
+
+bool MemoryMapVerifier::lastVerification() const
+{
+    return _lastVerification;
+}
+
+void MemoryMapVerifier::newNode(MemoryMapNode *currentNode)
+{
+    // Add an memory map interval watcher node to the init_task
+    if(currentNode->name() == "init_task") {
+        MemoryMapNodeIntervalWatcher *mw = new MemoryMapNodeIntervalWatcher(currentNode,
+                                                                           (*this),
+                                                                           0.05,
+                                                                           true,
+                                                                           false);
+        _watchNodes.append((MemoryMapNodeWatcher *)mw);
+    }
+}
+
+bool MemoryMapVerifier::performChecks(MemoryMapNode *lastNode)
+{
+    if(!verifyAddress(lastNode->address())) {
+        // If this node has no parent it was initiated based on a global var
+        // We do not print violations in those.
+        if(lastNode->parent() && lastNode->parent()->parent())
+            _log.warning(QString("Node '%1' with type %2 is not within the slub!")
+                         .arg(lastNode->fullName())
+                         .arg(lastNode->type()->prettyName()));
+    }
+
+    for(int i = 0; i < _watchNodes.size(); ++i) {
+        if(!_watchNodes.at(i)->check()) {
+            _log.warning(_watchNodes.at(i)->failMessage(lastNode));
+            _lastVerification = false;
+
+            if(_watchNodes.at(i)->getForceHalt()) {
+                debugmsg("Critical check failed. Stopping map generation.");
+                return false;
+            }
+        }
+    }
+
+    _lastVerification = true;
+    return true;
+}
+
+MemoryMapNode * MemoryMapVerifier::leastCommonAncestor(MemoryMapNode *a, MemoryMapNode *b)
+{
+    // If a is the parent of b or vice versa we return a or b respectively
+    if(b->parent() == a)
+        return a;
+    else if(a->parent() == b)
+        return b;
+    else if(a == b)
+        return a;
+
+    QList<MemoryMapNode *> *parentsA = a->getParents();
+    QList<MemoryMapNode *> *parentsB = b->getParents();
+    MemoryMapNode *result = NULL;
+    QListIterator<MemoryMapNode *> aIterator((*parentsA));
+    QListIterator<MemoryMapNode *> bIterator((*parentsB));
+
+    // The last elements should be equal otherwise the nodes have no common root node
+    if((parentsA->size() == 0 && a != parentsB->last()) ||
+            (parentsB->size() == 0 && b != parentsA->last()) ||
+            parentsA->last() != parentsB->last()) {
+        delete parentsA;
+        delete parentsB;
+
+        return result;
+    }
+    else
+        result = parentsA->size() > 0 ? parentsA->last() : a;
+
+    // Iterate over both list in reverse order and find the first node that is not
+    // contained in both lists. This is the least commonAncestor
+    aIterator.toBack();
+    bIterator.toBack();
+
+    while(aIterator.hasPrevious() && bIterator.hasPrevious())
+    {
+        // Check but do not advance both iterators
+        if(aIterator.peekPrevious() == bIterator.previous())
+            result = aIterator.previous();
+    }
+
+    delete parentsA;
+    delete parentsB;
+
+    return result;
+}
+
+/*
+ *  MemoryMapVerifier ===================================>
+ */
