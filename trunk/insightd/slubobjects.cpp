402339e73cfeb166a7ef351d9782afe111fb799c
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2012-07-27 16:03:11 +0000

    - Try to guess the BaseType that is stored in the slab caches. - Check the validity of a given Instance object accoring the the slab caches and global variables. - Moved some debug output from memorymap.cpp into this class.

--- trunk/insightd/slubobjects.cpp
+++ trunk/insightd/slubobjects.cpp
@@ -7,6 +7,7 @@
 #include "symfactory.h"
 #include "virtualmemory.h"
 #include "instance.h"
+#include "structured.h"
 #include "variable.h"
 
 SlubObjects::SlubObjects(const SymFactory* factory, VirtualMemory *vmem)
@@ -20,6 +21,83 @@ void SlubObjects::clear()
     _objects.clear();
     _caches.clear();
     _indices.clear();
+}
+
+
+SlubObject SlubObjects::objectAt(quint64 address) const
+{
+    if (_objects.isEmpty())
+        return SlubObject();
+    // Find the first object with an address less or equal the given address
+    AddressMap::const_iterator it = _objects.lowerBound(address);
+    AddressMap::const_iterator b = _objects.begin(), e = _objects.end();
+    while (it != b && (it == e || it.key() > address))
+        --it;
+    int i = it.value();
+    // Check if the address lies within that object
+    if (it.key() <= address &&
+        ((_caches[i].objSize <= 0 && it.key() == address) ||
+         (address < it.key() + _caches[i].objSize)))
+        return SlubObject(it.key(), _caches[i]);
+    else
+        return SlubObject();
+}
+
+
+SlubObjects::ObjectValidity SlubObjects::objectValid(const Instance *inst) const
+{
+    if (!inst || inst->isNull() || !inst->isValid())
+        return ovInvalid;
+
+    SlubObject obj(objectAt(inst->address()));
+    // Did we find an object at that address?
+    if (obj.isNull) {
+        // Do we find a global variable of that type?
+        VariableList vars = _factory->varsUsingId(inst->type()->id());
+        for (int i = 0; i < vars.size(); ++i)
+            if (vars[i]->offset() == inst->address())
+                return ovValid;
+        // Is this type acutally organized in slabs?
+        for (int i = 0; i < _caches.size(); ++i)
+            if (_caches[i].baseType &&
+                _caches[i].baseType->hash() == inst->type()->hash())
+                return ovNotFound;
+        return ovNoSlabType;
+    }
+    // If we don't know the slab object's type, we assume it's valid
+    else if (!obj.baseType)
+        return ovMaybeValid;
+    // Did we find the correct object?
+    else if (inst->address() == obj.address &&
+             obj.baseType->hash() == inst->type()->hash())
+        return ovValid;
+    // Check if inst is embedded within obj
+    else if (isInstanceEmbedded(inst, obj))
+        return ovEmbedded;
+    else
+        return ovConflict;
+}
+
+
+bool SlubObjects::isInstanceEmbedded(const Instance *inst,
+                                     const SlubObject &obj) const
+{
+    if (!inst || obj.isNull || !obj.baseType)
+        return false;
+    // Find base type at the expected offset
+    quint64 offset = obj.address - inst->address();
+    const StructuredMember* m = obj.baseType->memberAtOffset(offset);
+
+    if (m) {
+        const BaseType *it, *mt;
+        // Find final base type and compare them
+        if ((it = inst->type()->dereferencedBaseType(BaseType::trLexical)) &&
+            (mt = m->refTypeDeep(BaseType::trLexical)) &&
+            it->hash() == mt->hash())
+            return true;
+    }
+
+    return false;
 }
 
 
@@ -52,14 +130,38 @@ void SlubObjects::parsePreproc(const QString &fileName)
     in.close();
 
     postproc();
+
+    // Sort the caches by name
+    QStringList names(_indices.keys());
+    names.sort();
+
+    debugmsg("Read slub objects from file " << fileName << ":");
+    for (int i = 0; i < names.size(); ++i) {
+        int index = _indices[names[i]];
+        QString s = QString("%1. %2 objSize = %3, count = %4")
+                .arg(i+1, 3)
+                .arg(_caches[index].name, -30)
+                .arg(_caches[index].objSize, 4)
+                .arg(_caches[index].objects.size(), 4);
+        if (_caches[index].baseType)
+            s += " -> " + _caches[index].baseType->prettyName();
+        debugmsg(s);
+    }
+
 }
 
 
 void SlubObjects::postproc()
 {
+    resolveObjSize();
+    resolveBaseType();
+}
+
+
+void SlubObjects::resolveObjSize()
+{
     // Start with one arbitrary slub cache
-//    Variable* var = _factory->findVarByName("task_struct_cachep");
-    Variable* var = _factory->findVarByName("filp_cachep");
+    Variable* var = _factory->findVarByName("task_struct_cachep");
     if (!var) {
         debugerr("Could not find variable task_struct_cachep, skipping post-"
                  "processing.");
@@ -71,7 +173,6 @@ void SlubObjects::postproc()
         Instance start = var->toInstance(_vmem, BaseType::trLexicalAndPointers);
         Instance inst = start;
         do {
-//            inst = inst.dereference(BaseType::trLexicalAndPointers, 1);
             // Read name and size from memory
             name = inst.findMember("name", BaseType::trLexical).toString();
             name = name.remove('"');
@@ -82,9 +183,6 @@ void SlubObjects::postproc()
             if (_indices.contains(name)) {
                 int index = _indices[name];
                 _caches[index].objSize = size;
-            }
-            else if (name != "NULL") {
-                debugmsg("Did not find cache \"" << name << "\" in my list.");
             }
 
             // Proceed to next cache
@@ -100,6 +198,97 @@ void SlubObjects::postproc()
     catch (GenericException& e) {
         debugerr(e.message);
     }
+}
+
+
+void SlubObjects::resolveBaseType()
+{
+    // Try to guess the object type
+    QList<BaseType*> types;
+    for (int i = 0; i < _caches.size(); ++i) {
+        QString name = _caches[i].name;
+        // Search for types with name equal to the slab
+        if (name == "blkdev_queue")
+            name = "request_queue";
+        else if (name == "blkdev_ioc")
+            name = "io_context";
+        else if (name == "blkdev_requests")
+            name = "request";
+        else if (name == "cred_jar")
+            name = "cred";
+        else if (name == "eventpoll_epi")
+            name = "epitem";
+        else if (name == "eventpoll_pwq")
+            name = "eppoll_entry";
+        else if (name == "ext3_inode_cache")
+            name = "ext3_inode_info";
+        else if (name == "filp")
+            name = "file";
+        else if (name == "ip6_dst_cache")
+            name = "rt6_info";
+        else if (name == "ip_fib_hash")
+            name = "fib_node";
+        else if (name == "mnt_cache")
+            name = "vfsmount";
+        else if (name == "skbuff_head_cache")
+            name = "sk_buff";
+        else if (name == "sock_inode_cache")
+            name = "socket_alloc";
+        else if (name == "task_xstate")
+            name = "thread_xstate";
+        else if (name == "tcp_bind_bucket")
+            name = "inet_bind_bucket";
+        else if (name == "uid_cache")
+            name = "user_struct";
+        else if (name.endsWith("_cache"))
+            name = name.left(name.size() - qstrlen("_cache"));
+        else if (name.endsWith('s'))
+            name = name.left(name.size() - 1);
+
+        // Try exact match first
+        types.clear();
+        types.append(_factory->findBaseTypeByName(name));
+        if (testSlubTypes(types, _caches[i]))
+            continue;
+
+        // Next, try a wildcard search
+        name = QString("*%1*").arg(name.replace(QRegExp("[-_]+"), "*").replace("v6", "*6*"));
+        types = _factory->findBaseTypesByName(
+                    name, QRegExp::WildcardUnix, Qt::CaseInsensitive);
+        testSlubTypes(types, _caches[i]);
+    }
+}
+
+
+int SlubObjects::testSlubTypes(const QList<BaseType*>& types, SlubCache& cache)
+{
+    int count = 0;
+
+    for (int i = 0; i < types.size(); ++i) {
+        const BaseType* t = types[i];
+        if (!t)
+            continue;
+        if ((t->type() & StructOrUnion) &&
+            cache.objSize > 0 &&
+            t->size() == (uint)cache.objSize)
+        {
+            ++count;
+            if (cache.baseType) {
+                debugmsg("More than one matching type for "
+                         << cache.name << ": "
+                         << cache.baseType->prettyName()
+                         << " and " << t->prettyName());
+                // Take the one whose name length is closer to the
+                // cache's name
+                if (t->name().size() >= cache.baseType->name().size())
+                    continue;
+            }
+            cache.baseType =
+                    dynamic_cast<const Structured*>(t);
+        }
+    }
+
+    return count;
 }
 
 
--- trunk/insightd/slubobjects.h
+++ trunk/insightd/slubobjects.h
@@ -7,20 +7,44 @@
 
 class SymFactory;
 class VirtualMemory;
+class BaseType;
+class Structured;
+class Instance;
 
 typedef QMap<quint64, int> AddressMap;
 typedef QList<quint64> AddressList;
 
+/**
+ * This struct represents a SLUB cache (<tt>struct kmem_cache</tt>) that manages
+ * allocation of objects in the kernel.
+ */
 struct SlubCache
 {
-    SlubCache(const QString& name) : objSize(-1), name(name) {}
+    SlubCache(const QString& name) : objSize(-1), name(name), baseType(0) {}
     int objSize;
     QString name;
     AddressList objects;
+    const Structured* baseType;
 };
 
 typedef QHash<QString, int> NameIndexHash;
 typedef QList<SlubCache> CacheList;
+
+/**
+ * This struct represents an object in a SLUB cache.
+ */
+struct SlubObject
+{
+    SlubObject() : isNull(true), address(0), size(0), baseType(0) {}
+    SlubObject(quint64 address, const SlubCache& slub)
+        : isNull(false), address(address), size(slub.objSize),
+          slubName(slub.name), baseType(slub.baseType) {}
+    bool isNull;
+    quint64 address;
+    int size;
+    QString slubName;
+    const Structured* baseType;
+};
 
 /**
  * This class reads the debug output the SLUB allocator to identify allocated
@@ -55,16 +79,69 @@ typedef QList<SlubCache> CacheList;
 class SlubObjects
 {
 public:
+    /// Is a given instance valid according to the slab caches?
+    enum ObjectValidity {
+        ovInvalid,       ///< Instance is invalid, no further information avaliable
+        ovNoSlabType,    ///< Instance not found, but base type actually not present in any slab
+        ovNotFound,      ///< Instance not found, even though base type is managed in slabs
+        ovConflict,      ///< Instance type or address conflicts with object in the slabs
+        ovEmbedded,      ///< Instance is embedded within a larger object in the slabs
+        ovMaybeValid,    ///< Instance lies within reserved slab memory for which no type information is available
+        ovValid          ///< Instance was either found in he slabs or in a global variable
+    };
+
     SlubObjects(const SymFactory* factory, VirtualMemory* vmem);
+
+    /**
+     * Frees all data and resets all values to default.
+     */
     void clear();
+
+    /**
+     * Parses a pre-processed SLUB debug log file.
+     * @param fileName name of the file to read
+     */
     void parsePreproc(const QString& fileName);
+
 //    void parseRaw(const QString& fileName);
 
+    /**
+     * @return list of all SLUB caches
+     */
     const CacheList& caches() const;
+
+    /**
+     * Access function to all objects that are held in SLUB caches. The map
+     * is indexed by the objects' addresses mapped to on integer index that
+     * represents the index into the caches() list.
+     * @return map of SLUB objects as {address -> index}
+     * \sa caches()
+     */
     const AddressMap& objects() const;
 
+    /**
+     * Finds any object that occupies the memory at address \a addresse.
+     * @param address the virtual memory address to seach for
+     * @return the SlubObject at given address, if found, or an null object
+     * otherwise
+     */
+    SlubObject objectAt(quint64 address) const;
+
+    /**
+     * Checks if an Instance is valid according to the objects in the slabs or
+     * according to global variables.
+     * @param inst the instance to check
+     * @return an object validity code
+     * \sa ObjectValidity
+     */
+    ObjectValidity objectValid(const Instance* inst) const;
+
 private:
+    bool isInstanceEmbedded(const Instance* inst, const SlubObject& obj) const;
     void postproc();
+    void resolveObjSize();
+    void resolveBaseType();
+    int testSlubTypes(const QList<BaseType*>& types, SlubCache &cache);
     void addObject(const QString& name, QString addr, int lineNo = -1);
     void addObject(const QString& name, quint64 addr);
 
@@ -86,5 +163,7 @@ inline const AddressMap& SlubObjects::objects() const
 {
     return _objects;
 }
+
+
 
 #endif // SLUBOBJECTS_H
