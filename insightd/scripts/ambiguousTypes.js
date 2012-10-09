d57c8b6f4ccf79c4f8443b68d1a19eb837d17e80
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2012-10-09 13:06:04 +0000

    Don't fail on errors.

--- insightd/scripts/ambiguousTypes.js
+++ insightd/scripts/ambiguousTypes.js
@@ -18,41 +18,62 @@ println(hline(w_total));
 // Get all type names
 var typeNames = Symbols.typeNames();
 var count = 0;
+var errors = new Array;
 
 for (var i in typeNames) {
 	// Find all types with that name
 	var types = Symbols.listTypes(typeNames[i]);
 	var sizes = new Array;
 
-	for (var t in types) {
-		// Skip types with zero length
-		if (!types[t].Size())
-			continue;
-		// Skip functions
-		if (types[t].Type() == "Function")
-			continue;
-		// Check if we already have that size
-		var found = false;
-		for (var s in sizes) {
-			if (sizes[s] == types[t].Size()) {
-				found = true;
-				break;
-			}
-		}
-		if (!found)
-			sizes.push(types[t].Size());
-	}
+    for (var t in types) {
+        try {
+            // Skip types with zero length
+            if (!types[t].Size())
+                continue;
+            // Skip functions
+            if (types[t].Type() == "Function")
+                continue;
+            // Check if we already have that size
+            var found = false;
+            for (var s in sizes) {
+                if (sizes[s] == types[t].Size()) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                sizes.push(types[t].Size());
+        }
+        catch (e) {
+            errors.push("### Type 0x" + types[t].TypeId().toString(16) + ": " + e);
+            continue;
+        }
+    }
 
-	if (sizes.length > 1) {
-		sizes.sort();
-        println(
-			lalign(types[0].TypeName(), w_name) + " " +
-			lalign(types[0].Type(), w_type) + " " +
-			ralign(sizes.join(", "), w_size)
-		);
-		count++;
-	}
+    if (sizes.length > 1) {
+        try {
+            sizes.sort();
+            println(
+                lalign(types[0].TypeName(), w_name) + " " +
+                lalign(types[0].Type(), w_type) + " " +
+                ralign(sizes.join(", "), w_size)
+            );
+            count++;
+        }
+        catch (e) {
+            errors.push("### Type 0x" + types[t].TypeId().toString(16) + ": " + e);
+            continue;
+        }
+    }
 }
 
 println(hline(w_total));
 println("Total: " + count + " ambiguous types");
+
+if (errors.length > 0) {
+    println();
+    println("A total of", errors.length, "errors occured:");
+    for (var i in errors)
+        println(errors[i]);
+}
+
