133d6e6104ebbf39cfe3b909c359eb67f99c4568
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2012-11-07 13:36:27 +0000

    Belongs to r1509: Extracted AltRefType out of ReferencingType. This class now has its separate definition/implementation.

--- trunk/insightd/referencingtype.h
+++ trunk/insightd/referencingtype.h
@@ -11,6 +11,7 @@
 #include "typeinfo.h"
 #include "instance.h"
 #include "basetype.h"
+#include "altreftype.h"
 
 class SymFactory;
 class ASTExpression;
@@ -19,78 +20,6 @@ class ASTVariableExpression;
 class ReferencingType
 {
 public:
-    class AltRefType
-    {
-    public:
-        /**
-         * Constructor
-         * @param id the ID of the target BaseType
-         * @param expr the ASTExpression to compute the target type's address
-         */
-        explicit AltRefType(int id = -1, const ASTExpression* expr = 0);
-
-        /**
-         * Checks if the given Instance \a inst is compatible with the
-         * expression that needs to be evaluated for this alternative type.
-         * Therefore all ASTVariableExpressions within the expression are
-         * checked for type compatibility with \a inst.
-         * @param inst the Instance object to check against the expression
-         * @return \c true if \a inst is compatible to the expression, \c false
-         * otherwise
-         */
-        bool compatible(const Instance *inst) const;
-
-        /**
-         * Creates an Instance of this alternative type.
-         * @param vmem the VirtualMemory object to create the instance for
-         * @param inst the Instance object to evaluate expr with
-         * @param factory the factory to which this type belongs
-         * @param name the name for this instance, i.e., variable or member name
-         * @param parentNames the parent name components used to reach this
-         * alternative type
-         */
-        Instance toInstance(VirtualMemory* vmem, const Instance* inst,
-                            const SymFactory* factory, const QString &name,
-                            const QStringList &parentNames) const;
-
-        /**
-         * Reads a serialized version of this object from \a in.
-         * \sa writeTo()
-         * @param in the data stream to read the data from, must be ready to read
-         * @param factory the factory holding the symbols
-         */
-        void readFrom(KernelSymbolStream& in, SymFactory *factory);
-
-        /**
-         * Writes a serialized version of this object to \a out
-         * \sa readFrom()
-         * @param out the data stream to write the data to, must be ready to write
-         */
-        void writeTo(KernelSymbolStream &out) const;
-
-        /**
-         * @return the ID of the target BaseType
-         */
-        inline int id() const
-        {
-            return _id;
-        }
-
-        /**
-         * @return the ASTExpression to compute the target type's address
-         */
-        inline const ASTExpression* expr() const
-        {
-            return _expr;
-        }
-
-    private:
-        void updateVarExpr();
-        int _id;
-        const ASTExpression* _expr;
-        QList<const ASTVariableExpression*> _varExpr;
-    };
-
     /**
      * Constructor
      */
@@ -355,12 +284,12 @@ inline int ReferencingType::altRefTypeCount() const
 }
 
 
-inline QList<ReferencingType::AltRefType>& ReferencingType::altRefTypes()
+inline QList<AltRefType>& ReferencingType::altRefTypes()
 {
     return _altRefTypes;
 }
 
-inline const QList<ReferencingType::AltRefType>& ReferencingType::altRefTypes() const
+inline const QList<AltRefType>& ReferencingType::altRefTypes() const
 {
     return _altRefTypes;
 }
