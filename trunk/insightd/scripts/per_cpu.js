f8c6716ee218976cae5b42c1a18481f1a0f7c678
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2012-11-19 15:28:40 +0000

    New and streamlined rules.

--- trunk/insightd/scripts/per_cpu.js
+++ trunk/insightd/scripts/per_cpu.js
@@ -1,6 +1,5 @@
 /*
 Generic function to dereference a per-cpu member within a struct/union.
-It is assumed that only one member is accessed!
 
 References:
 http://lxr.linux.no/#linux+v2.6.32.58/include/linux/percpu-defs.h#L8
@@ -9,7 +8,26 @@ http://lxr.linux.no/#linux+v2.6.32.58/include/asm-generic/percpu.h#L54
 */
 function per_cpu_member(inst, members)
 {
-	inst = inst.Member(members[0]);
+	for (var i in members) {
+		inst = inst.Member(members[i]);
+		if (!inst.IsValid() || inst.IsNull())
+			return false;
+	}
+	return per_cpu_var(inst);
+}
+
+/*
+Generic function to dereference a per-cpu variable.
+
+References:
+http://lxr.linux.no/#linux+v2.6.32.58/include/linux/percpu-defs.h#L8
+http://lxr.linux.no/#linux+v2.6.32.58/include/asm-generic/percpu.h#L18
+http://lxr.linux.no/#linux+v2.6.32.58/include/asm-generic/percpu.h#L54
+*/
+function per_cpu_var(inst)
+{
+	if (!inst.IsValid() || inst.IsNull())
+		return false;
 	var offsets = new Instance("__per_cpu_offset");
 	// Is this an SMP system?
 	if (offsets.IsValid()) {
