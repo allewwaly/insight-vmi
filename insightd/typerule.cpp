db44f927136fdb492fef0e4bc6858c039ab353f1
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2012-11-25 20:34:19 +0000

    Check if the instances returned by rules point back to the given instance or overlap with it. In that case, we return the given instance's member that was originally pointed to.

--- insightd/typerule.cpp
+++ insightd/typerule.cpp
@@ -665,7 +665,50 @@ Instance ExpressionAction::evaluate(const Instance *inst,
         if (!members[i]->name().isEmpty())
             parentNames += members[i]->name();
 
-    return _altRefType.toInstance(inst->vmem(), inst, fac, name, parentNames);
+    Instance ret(_altRefType.toInstance(inst->vmem(), inst, fac, name,
+                                        parentNames));
+
+    // Check for pointers that point back into or overlap with "inst"
+    bool overlap;
+    if (ret.address() < inst->address())
+        overlap = (inst->address() <= ret.endAddress());
+    else
+        overlap = (ret.address() <= inst->endAddress());
+
+    if (overlap && !members.isEmpty()) {
+        // This is most likely an empty list-like structure, so return the
+        // member where the address points to
+        const BaseType* rt;
+        quint64 realAddr = inst->address();
+        ret = *inst;
+        Instance tmp;
+        // Find out where the pointer points to natively (without expressions)
+        for (int i = 0; i < members.size(); ++i) {
+            tmp = ret.member(members[i]->index(), 0, 0, ksNone);
+            if (tmp.isValid())
+                ret = tmp.dereference(BaseType::trLexical);
+            else
+                break;
+        }
+        // Is the last member a pointer or numeric type?
+        if (ret.type()->type() & (rtPointer|rtInt32|rtInt64|rtUInt32|rtUInt64))
+            realAddr = (quint64)ret.toPointer();
+
+        // Now find the member of inst where the real address points to
+        ret = *inst;
+        int i = 0;
+        // Resolve the members as long as they are structs/unions
+        while (i < members.size() &&
+               ret.address() + members[i]->offset() >= realAddr &&
+               (rt = members[i]->refTypeDeep(BaseType::trLexical)) &&
+               (rt->type() & StructOrUnion))
+        {
+            ret = ret.member(members[i]->index(), 0, 0, ksNone);
+            ++i;
+        }
+    }
+
+    return ret;
 }
 
 
