2a6859c17f99444317bd4d55c7d0d4d139a96674
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2013-01-16 12:03:59 +0000

    Added method Enum::enumValue() to retrieve constant value of an enumerator.

--- insightd/enum.h
+++ insightd/enum.h
@@ -18,6 +18,8 @@ class Enum: public BaseType
 {
 public:
 	typedef QMultiHash<qint32, QString> EnumHash;
+	typedef QMultiHash<typename EnumHash::mapped_type, typename EnumHash::key_type>
+		EnumValueHash;
 
 	/**
 	 * Constructor
@@ -57,13 +59,22 @@ public:
 	/**
 	 * @return the names and values of the defined enumerators
 	 */
-    const EnumHash& enumValues() const;
+	const EnumHash& enumerators() const;
 
 	/**
 	 * Sets the names and values of the defined enumerators.
 	 * @param values new enumerators
 	 */
-	void setEnumValues(const EnumHash& values);
+	void setEnumerators(const EnumHash& values);
+
+	/**
+	 * Returns the integer value of the given enumerator. If it does not exist,
+	 * an excpetion is thrown
+	 * @param enumerator the name of the enumerator
+	 * @return the associated value
+	 * @throws BaseTypeException if \a enumerator does not exist
+	 */
+	int enumValue(const QString& enumerator) const;
 
     /**
      * Reads a serialized version of this object from \a in.
@@ -80,22 +91,14 @@ public:
     virtual void writeTo(KernelSymbolStream& out) const;
 
 protected:
-	EnumHash _enumValues;
+	EnumHash _enumerators;
+	EnumValueHash _enumValues;
 };
 
 
-inline const Enum::EnumHash& Enum::enumValues() const
+inline const Enum::EnumHash& Enum::enumerators() const
 {
-    return _enumValues;
+	return _enumerators;
 }
-
-
-inline void Enum::setEnumValues(const EnumHash& values)
-{
-    _enumValues = values;
-    _hashValid  = false;
-}
-
-
 
 #endif /* ENUM_H_ */
