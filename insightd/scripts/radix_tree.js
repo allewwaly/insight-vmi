ee3d9765db8601d8f5abec77a0e5fcf84b5ab81e
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2013-01-17 15:40:31 +0000

    Fixed the rules and added consistancy checks for the radix_tree_node objects.

--- insightd/scripts/radix_tree.js
+++ insightd/scripts/radix_tree.js
@@ -5,6 +5,8 @@
 // Reference: http://lxr.linux.no/#linux+v2.6.32.58/include/linux/radix-tree.h#L38
 const RADIX_TREE_INDIRECT_PTR = 1;
 
+const RADIX_TREE_MAX_HEIGHT = 6;
+
 /*
 Generic function to retrieve the radix_tree_node from an (embedded) 
 radix_tree_root
@@ -13,19 +15,30 @@ References:
 http://lxr.linux.no/#linux+v2.6.32.58/include/linux/radix-tree.h#L61
 http://lxr.linux.no/#linux+v2.6.32.58/include/linux/radix-tree.h#L48
 */
-function radix_tree_node_from_root(inst, members)
+function radix_tree_node_from_root(root, members)
 {
+	// With a height of 0, the tree is empty and the pointer invalid
+	var h = root.height;
+	if (h == 0 || h > RADIX_TREE_MAX_HEIGHT)
+		return new Instance();
 	// Final member is "rnode" of "struct radix_tree_root"
+	var node = root;
 	for (var i in members) {
-		inst = inst.Member(members[i]);
-		if (!inst.IsValid() || inst.IsNull())
+		node = node.Member(members[i]);
+		if (!node.IsValid() || node.IsNull())
 			return false;
 	}
 	// Do we need to unset the INDIRECT_PTR flag?
-	var addr_low = inst.AddressLow();
+	var addr_low = node.AddressLow();
 	if (addr_low & RADIX_TREE_INDIRECT_PTR)
-		inst.SetAddressLow(addr_low & ~RADIX_TREE_INDIRECT_PTR);
-	return inst;
+		node.SetAddressLow(addr_low & ~RADIX_TREE_INDIRECT_PTR);
+	// Is the node consistent?
+	if (node.IsAccessible() && (node.height > RADIX_TREE_MAX_HEIGHT || node.count > node.slots.ArrayLength())) {
+		println("Found inconsistent radix_tree_node @ 0x" + node.Address() +
+			", radix_tree_root is", root.FullName(), "@ 0x" + root.Address());
+		return new Instance();
+	}
+	return node;
 }
 
 /*
@@ -33,14 +46,12 @@ Handles radix tree nodes according to their height and item type
 */ 
 function radix_tree_node_slots(node, members)
 {
-	var slots = node.Member(members[0]);
-	// Nodes with a height of null are leaves, all others are interior nodes
-	if (node.height > 0) {
-		return radix_tree_deref_slots(slots, node.TypeId(), node.treeItemType);
-	}
+	// Nodes with a height of 1 are leaves, all others are interior nodes
+	if (node.height > 1)
+		return radix_tree_deref_slots(node, node.TypeId());
 	// The leaves store "struct page" objects
-	else if (node.height == 0)
-		return radix_tree_deref_slots(slots, "page");
+	else if (node.height == 1)
+		return radix_tree_deref_slots(node, "page");
 	// This seems to be an invalid node anyway
 	else
 		return false;
@@ -49,24 +60,43 @@ function radix_tree_node_slots(node, members)
 /*
 Dereferences the slots of radix tree node as the given typeId
 */
-function radix_tree_deref_slots(slots, typeId, treeItemType)
+function radix_tree_deref_slots(node, typeId)
 {
-	var len = slots.ArrayLength();
+	var slots = node.slots;
 	var ret = new Array();
+	const len = slots.ArrayLength();
+
+	var cnt = 0;
 	for (var i = 0; i < len; ++i) {
 		var e = slots.ArrayElem(i);
 		// Dereference once
 		if (e.IsAccessible()) {
 			e.SetAddress(e.toPointer());
+			// Count the number of non-null objects
+			if (!e.IsNull())
+				cnt++;
+			else
+				continue;
 			// Make sure the address is no indirect pointer
 			if (e.AddressLow() & RADIX_TREE_INDIRECT_PTR)
 				continue;
 			e.ChangeType(typeId);
-			// Preserve the tree type, if given
-			if (treeItemType)
-				e.treeItemType = treeItemType;
+            // For a radix_tree_node, a A valid height is not larger than 6, a
+			// valid count can't be larger than len
+			if (typeId == node.TypeId() && (e.height > 6 || e.count > len)) {
+				println("Found inconsistent radix_tree_node in slot", i, 
+						"@ 0x" + e.Address() + ", parent is", node.FullName(), 
+						"@ 0x" + node.Address());
+				continue;
+			}
 			ret.push(e);
 		}
+	}
+	
+	// Did we find more nodes than the counter indicates?
+	if (cnt != node.count) {
+		println("Found", cnt, "non-null slots in radix_tree_node, but the count is", node.count, 
+				"for", node.FullName(), "@ 0x" + node.Address());
 	}
 	return ret;
 }
