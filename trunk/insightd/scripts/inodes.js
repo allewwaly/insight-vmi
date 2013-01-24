9d4326c694a9b3ee9fa6cfc43682de57bd05f7a0
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2013-01-24 14:59:26 +0000

    - Distinguish inode types based on the super block's operations. - Added support for bdev_inode type.

--- trunk/insightd/scripts/inodes.js
+++ trunk/insightd/scripts/inodes.js
@@ -1,15 +1,19 @@
 /*
-Compares the super_block of the given inode against other super_block
-objects and changes the inode's type to the embedding type, if the type is known.
+Compares characteristic pointers of the given inode against other well-known
+objects, such as the super_block operations. If the type is known, the inode's
+type is changed to the embedding type.
 */
 function inode_resolve_embedding(inode)
 {
-	var sb = inode.i_sb;
+	// The most reliable distinction is using the related super_operations
+	var sb_op = inode.i_sb.s_op;
+	if (sb_op === undefined || sb_op.IsNull())
+		return inode;
 
 	// Is this a "struct shmem_inode_info"?
 	// See: http://lxr.linux.no/#linux+v2.6.32/include/linux/shmem_fs.h#L36
-	var shm_sb = new Instance("shm_mnt.mnt_sb");
-	if (sb.Address() == shm_sb.Address()) {
+	var shm_sb_op = new Instance("shmem_ops");
+	if (sb_op.Address() == shm_sb_op.Address()) {
 		inode.ChangeType("shmem_inode_info");
 		inode.AddToAddress(-inode.MemberOffset("vfs_inode"));
 		return inode;
@@ -17,9 +21,17 @@ function inode_resolve_embedding(inode)
 	
 	// Is this a "struct proc_inode"?
 	// See: http://lxr.linux.no/#linux+v2.6.32/include/linux/proc_fs.h#L274
-	var proc_sb = new Instance("proc_mnt.mnt_sb");
-	if (sb.Address() == proc_sb.Address()) {
+	var proc_sb_op = new Instance("proc_sops");
+	if (sb_op.Address() == proc_sb_op.Address()) {
 		inode.ChangeType("proc_inode");
+		inode.AddToAddress(-inode.MemberOffset("vfs_inode"));
+		return inode;
+	}
+	
+	// Is this a "struct bdev_inode"?
+	var bdev_sb_op = new Instance("bdev_sops");
+	if (sb_op.Address() == bdev_sb_op.Address()) {
+		inode.ChangeType("bdev_inode");
 		inode.AddToAddress(-inode.MemberOffset("vfs_inode"));
 		return inode;
 	}
