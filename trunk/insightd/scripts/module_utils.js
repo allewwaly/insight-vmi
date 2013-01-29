3558410be7c76b1de8193765129612703356d9d5
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2013-01-29 08:48:41 +0000

    Added utility function that retrieves a module variable if its section is known.

--- trunk/insightd/scripts/module_utils.js
+++ trunk/insightd/scripts/module_utils.js
@@ -28,26 +28,67 @@ function print_module_sections(m)
 }
 
 
-function print_module_var(v, section_name)
+function get_module_var(var_name, section_name)
 {
-	if (!v.IsValid())
+	// Disable the rules engine
+	var useRulesSave = Instance.useRules;
+	Instance.useRules = false;
+	var v = new Instance(var_name);
+	
+	if (!v.IsValid()) {
+		Instance.useRules = useRulesSave;
 		return v;
+	}
+	
+	// Enable the engine, we need it from now on
+	Instance.useRules = true;
 	
 	// Extract module name
 	var fileName = v.OrigFileName();
-	if (fileName == "vlinux" || fileName == "")
-		println(v);
+	if (fileName == "vmlinux")
+		throw("Variable '" + var_name + "' belongs to the kernel.");
 	var moduleName = fileName.substring(fileName.lastIndexOf("/")+1, fileName.lastIndexOf("."));
+	
 	// Find the corresponding module structure
 	var modules = new Instance("modules");
 	var m = modules.next;
+	var found = false;
 	while (modules.Address() != m.Address() && modules.Address() != m.list.Address()) {
 		if (m.name == moduleName) {
-			println("Found module: " + m.name);
+			found = true;
 			break;
 		}
 		m = m.list.next;		
 	}
 	
-	// To be continued
+	// Throw an error if we didn't find the module
+	if (!found || !m.IsValid()) {
+		Instance.useRules = useRulesSave;
+		throw("Module '" + moduleName + "' not found or not loaded.");
+	}
+	
+	// Find the section with the given name	
+	found = false;
+	var cnt = m.sect_attrs.nsections;
+	var attrs = m.sect_attrs.attrs;
+	for (var i = 0; i < cnt; ++i) {
+		var attr = attrs.ArrayElem(i);
+		if (attr.name == section_name) {
+			v.AddToAddress(attr.address.toLong(16));
+			found = true;
+			break;
+		}
+	}
+	
+	Instance.useRules = useRulesSave;
+	if (found)
+		return v;
+	else
+		throw("Section '" + section_name + "' not found for module '" + moduleName + "'.");
+}
+
+
+function print_module_var(var_name, section_name)
+{
+	println(get_module_var(var_name, section_name));
 }
