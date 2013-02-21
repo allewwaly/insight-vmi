468125c1643214f14faaa54f1321cbd67aa15256
chrschn.de@googlemail.com <chrschn.de@googlemail.com@3c4e7505-9c17-2a5b-3de6-9722c72d0129>
2013-02-21 15:43:11 +0000

    Paint objects in the color of their validity (rather than their probability).

--- trunk/insightd/memorymapwidget.cpp
+++ trunk/insightd/memorymapwidget.cpp
@@ -25,30 +25,64 @@
 #include <insight/varsetter.h>
 #include <debug.h>
 
+
 static const int margin = 3;
 static const int cellSize = 9;
+
 static const unsigned char PROB_MAX = 16;
+enum ColorIndicies {
+    ccProbMax   = PROB_MAX,
+    ccFunction,
+    ccGlobalVar,
+    ccValid,
+    ccInvalid,
+    ccUnknown,
+    ccCount
+};
 
-static const QColor funcColor(0, 128, 255);
+static const QColor cellColors[ccCount] = {
+    // Probabilities
+    QColor(255,   0,   0),  //  0
+    QColor(255,  16,   0),  //  1
+    QColor(255,  32,   0),  //  2
+    QColor(255,  48,   0),  //  3
+    QColor(255,  64,   0),  //  4
+    QColor(255,  80,   0),  //  5
+    QColor(255,  96,   0),  //  6
+    QColor(255, 112,   0),  //  7
+    QColor(255, 128,   0),  //  8
+    QColor(224, 128,   0),  //  9
+    QColor(192, 128,   0),  // 10
+    QColor(160, 128,   0),  // 11
+    QColor(128, 128,   0),  // 12
+    QColor( 96, 128,   0),  // 13
+    QColor( 64, 128,   0),  // 14
+    QColor( 32, 128,   0),  // 15
+    QColor(  0, 128,   0),  // 16
+    // Validities
+    QColor(0,   128, 255), // ccFunction
+    QColor(0,     0, 255), // ccGlobalVar
+    QColor(0,   128,   0), // ccValid
+    QColor(255,   0,   0), // ccInvalid
+    QColor(160, 160, 160)  // ccUnknown
+};
 
-static const QColor probColor[PROB_MAX+1] = {
-        QColor(255,   0, 0),  //  0
-        QColor(255,  16, 0),  //  1
-        QColor(255,  32, 0),  //  2
-        QColor(255,  48, 0),  //  3
-        QColor(255,  64, 0),  //  4
-        QColor(255,  80, 0),  //  5
-        QColor(255,  96, 0),  //  6
-        QColor(255, 112, 0),  //  7
-        QColor(255, 128, 0),  //  8
-        QColor(224, 128, 0),  //  9
-        QColor(192, 128, 0),  // 10
-        QColor(160, 128, 0),  // 11
-        QColor(128, 128, 0),  // 12
-        QColor( 96, 128, 0),  // 13
-        QColor( 64, 128, 0),  // 14
-        QColor( 32, 128, 0),  // 15
-        QColor(  0, 128, 0)   // 16
+
+struct Cell
+{
+    Cell() : addrStart(0), addrEnd(0), index(-1)
+    {
+        resetColors();
+    }
+
+    void resetColors()
+    {
+        for (int i = 0; i < 4; ++i)
+            colors[i] = -1;
+    }
+
+    quint64 addrStart, addrEnd, index;
+    int colors[4];
 };
 
 //------------------------------------------------------------------------------
@@ -157,7 +191,6 @@ void MemoryMapWidget::paintEvent(QPaintEvent * e)
 {
     // Set _isPainting to true now and to false on exit
     if (_isPainting) {
-        debugmsg("Ignoring paint event");
         e->ignore();
         return;
     }
@@ -165,7 +198,6 @@ void MemoryMapWidget::paintEvent(QPaintEvent * e)
     VarSetter<bool> painting(&_isPainting, true, false);
 
     static const QColor unusedColor(255, 255, 255);
-//    static const QColor usedColor(0, 0, 0);
     static const QColor gridColor(240, 240, 240);
     static const QColor bgColor(255, 255, 255);
     static const QColor userLandBgColor(220, 220, 220);
@@ -207,20 +239,23 @@ void MemoryMapWidget::paintEvent(QPaintEvent * e)
     // Draw all used parts
     painter.setBrush(Qt::NoBrush);
     painter.setPen(Qt::NoPen);
-    int lastColor = -1;
-    bool diffWasEmpty = true, propsWasEmpty = true, probsHadFunctions = false;
+
     bool sameAddrSpaceSize =
             !_map || !_diff || _map->addrSpaceEnd() == _diff->addrSpaceEnd();
 
     // State of current memory cell
-    struct {
-        quint64 addrStart, addrEnd, index;
-        unsigned char maxProb, minProb;
-    } cell;
+    Cell cell;
 
     cell.addrEnd = visibleAddrSpaceStart() - 1;
 
     const int totalCells = _cols * _rows;
+    int lastColor = -1;
+
+#define set_brush(c) \
+    if (lastColor != (c)) { \
+        painter.setBrush(cellColors[c]); \
+        lastColor = (c); \
+    }
 
     for (int i = 0; i < totalCells; ++i) {
 
@@ -236,123 +271,134 @@ void MemoryMapWidget::paintEvent(QPaintEvent * e)
                 _map->propertiesOfRange(cell.addrStart, cell.addrEnd) :
                 MemMapProperties();
 
-        MemMapSet nodes = _map ?
-                _map->objectsInRange(cell.addrStart, cell.addrEnd) :
-                MemMapSet();
-
         DiffProperties diffProps = _diff && sameAddrSpaceSize ?
                 _diff->propertiesOfRange(cell.addrStart, cell.addrEnd) :
                 DiffProperties();
 
-#ifdef DEBUG
-        if (props.isEmpty() != nodes.isEmpty()) {
-            debugmsg("Cell " << i << " "
-                    << QString("0x%1 - 0x%2")
-                        .arg(cell.addrStart, 8, 16, QChar('0'))
-                        .arg(cell.addrEnd, 8, 16, QChar('0'))
-                    << ": props.objectCount = "
-                    << props.objectCount << ", nodes.size() = " << nodes.size());
-
-#   ifdef ENABLE_DOT_CODE
-            QString filename = QString("subtree_0x%1_0x%2.dot")
-                                .arg(cell.addrStart, 8, 16, QChar('0'))
-                                .arg(cell.addrEnd, 8, 16, QChar('0'));
-            _map->outputSubtreeDotFile(cell.addrStart, cell.addrEnd, filename);
-#   endif
-
-//            props = _map->propertiesOfRange(cell.addrStart, cell.addrEnd);
-//            nodes = _map->objectsInRange(cell.addrStart, cell.addrEnd);
-        }
-#endif
-
         if (props.isEmpty() && diffProps.isEmpty())
             continue;
 
-        cell.minProb = (unsigned char)(PROB_MAX * props.minProbability);
-        cell.maxProb = (unsigned char)(PROB_MAX * props.maxProbability);
-        bool hasFunction = props.baseTypes & rtFunction;
-        bool hasObjects = props.baseTypes & ~rtFunction;
+        cell.resetColors();
+//        getCellProbabilityColors(&cell, props);
+        getCellValidityColors(&cell, props);
 
         // Draw  cell
         quint64 r = cell.index / _cols;
         quint64 c = cell.index % _cols;
 
+        // Anything to paint?
+        if (cell.colors[0] < 0) {
+            lastColor = -1;
+            continue;
+        }
+
         if (cellSize == 1) {
-            // TODO draw diff as well
-            if (cell.maxProb != lastColor) {
-                lastColor = cell.maxProb;
-                painter.setPen(probColor[lastColor]);
+            // If we can only paint one pixel, then use only one color
+            if (lastColor != cell.colors[0]) {
+                painter.setPen(cellColors[cell.colors[0]]);
+                lastColor = cell.colors[0];
             }
             painter.drawPoint(c, r);
         }
         else {
-            // Do we need to change the brush?
-            if (propsWasEmpty != props.isEmpty() || cell.maxProb != lastColor ||
-                probsHadFunctions)
-            {
-                if (props.isEmpty())
-                    painter.setBrush(Qt::NoBrush);
-                else {
-                    lastColor = cell.maxProb;
-                    painter.setBrush(probColor[lastColor]);
-                }
-                propsWasEmpty = props.isEmpty();
+            // Calculate coordinates
+            int x1, y1, w, h;
+            if (diffProps.isEmpty()) {
+                x1 = c * cellSize;
+                y1 = r * cellSize;
+                w = h = cellSize - 1;
             }
-            // Do we need to change the pen?
-            if (diffWasEmpty != diffProps.isEmpty()) {
-                if (diffProps.isEmpty())
-                    painter.setPen(Qt::NoPen);
-                else
-                    painter.setPen(diffColor);
-                diffWasEmpty = diffProps.isEmpty();
+            else {
+                x1 = c * cellSize - 1;
+                y1 = r * cellSize - 1;
+                w = h = cellSize;
             }
 
-            // Finally draw the cell
-            if (hasFunction && !hasObjects) {
-                lastColor = 255;
-                painter.setBrush(funcColor);
-                if (diffProps.isEmpty())
-                    painter.drawRect(c * cellSize, r * cellSize,
-                            cellSize - 1, cellSize - 1);
-                else
-                    painter.drawRect(c * cellSize - 1, r * cellSize - 1,
-                            cellSize, cellSize);
-            }
-            else if (hasObjects) {
-                if (diffProps.isEmpty())
-                    painter.drawRect(c * cellSize, r * cellSize,
-                            cellSize - 1, cellSize - 1);
-                else
-                    painter.drawRect(c * cellSize - 1, r * cellSize - 1,
-                            cellSize, cellSize);
+            if (cell.colors[0] >= 0) {
+                set_brush(cell.colors[0]);
+                painter.drawRect(x1, y1, w, h);
 
-                if (hasFunction) {
-                    lastColor = 255;
-                    painter.setBrush(funcColor);
+                if (cell.colors[1] >= 0) {
+                    // We need to draw grid lines
                     painter.setPen(gridColor);
-                    if (diffProps.isEmpty()) {
-                        QPoint points[3] = {
-                            QPoint(c * cellSize, r * cellSize),
-                            QPoint(c * cellSize, r * cellSize + (cellSize - 1)),
-                            QPoint(c * cellSize + (cellSize - 1), r * cellSize + (cellSize - 1)),
+                    set_brush(cell.colors[1]);
+                    // We need to enlarge the cell size now because of the pen
+                    QPoint points[3] = {
+                        QPoint(x1-1, y1-1),
+                        QPoint(x1-1, y1 + h),
+                        QPoint(x1 + w, y1 + h)
+                    };
+                    painter.drawConvexPolygon(points, 3);
+
+                    if (cell.colors[2] >= 0) {
+                        set_brush(cell.colors[2]);
+                        painter.setBrush(cellColors[cell.colors[2]]);
+                        QPoint points4[4] = {
+                            QPoint(x1-1, y1 + h),
+                            QPoint(x1-1 + (w>>1), y1 + ((h+1)>>1)),
+                            QPoint(x1 + ((w+1)>>1), y1 + ((h+1)>>1)),
+                            QPoint(x1 + w, y1 + h),
                         };
-                        painter.drawConvexPolygon(points, 3);
+
+                        painter.drawConvexPolygon(points4, 4);
+
+                        if (cell.colors[3] >= 0) {
+                            set_brush(cell.colors[3]);
+                            points4[0] = QPoint(x1-1, y1-1);
+                            points4[1] = QPoint(x1 + (w>>1), y1 + (h>>1));
+                            points4[2] = QPoint(x1 + ((w+1)>>1), y1 + (h>>1));
+                            points4[3] = QPoint(x1 + w, y1 - 1);
+                            painter.drawConvexPolygon(points4, 4);
+                        }
                     }
-                    else {
-                        QPoint points[3] = {
-                            QPoint(c * cellSize - 1, r * cellSize - 1),
-                            QPoint(c * cellSize - 1, (r * cellSize - 1) + (cellSize - 1)),
-                            QPoint((c * cellSize - 1) + cellSize, (r * cellSize - 1) + cellSize),
-                        };
-                        painter.drawConvexPolygon(points, 3);
-                    }
+                    // Disable gridlines again
                     painter.setPen(Qt::NoPen);
                 }
             }
         }
-
-        probsHadFunctions = hasFunction;
     }
+}
+
+
+void MemoryMapWidget::getCellProbabilityColors(Cell *cell,
+                                               const MemMapProperties &props)
+{
+    int numColors = 0;
+    if (props.baseTypes & rtFunction)
+        cell->colors[numColors++] = ccFunction;
+    if (props.baseTypes & ~rtFunction)
+        cell->colors[numColors++] = (unsigned char)(PROB_MAX * props.maxProbability);
+}
+
+
+void MemoryMapWidget::getCellValidityColors(struct Cell *cell,
+                                            const MemMapProperties &props)
+{
+    static const int validMask =
+            SlubObjects::ovEmbedded |
+            SlubObjects::ovEmbeddedCastType |
+            SlubObjects::ovEmbeddedUnion |
+            SlubObjects::ovValid |
+            SlubObjects::ovValidCastType;
+
+    static const int invalidMask =
+            SlubObjects::ovConflict |
+//            SlubObjects::ovInvalid |
+            SlubObjects::ovNotFound;
+
+    // Which colors to draw?
+    int numColors = 0;
+    if (props.baseTypes & rtFunction)
+        cell->colors[numColors++] = ccFunction;
+//    if (props.slubValidities & SlubObjects::ovValidGlobal)
+//        colors[numColors++] = vcGlobalVar;
+    if (props.slubValidities & validMask)
+        cell->colors[numColors++] = ccValid;
+    if (props.slubValidities & invalidMask)
+        cell->colors[numColors++] = ccInvalid;
+    if (numColors < 4 &&
+        (props.slubValidities & ~(validMask|invalidMask /*|SlubObjects::ovValidGlobal*/ )))
+        cell->colors[numColors++] = ccUnknown;
 }
 
 
@@ -488,8 +534,8 @@ bool MemoryMapWidget::event(QEvent *event)
         }
 
         return true;
-     }
-     return QWidget::event(event);
+    }
+    return QWidget::event(event);
 }
 
 
